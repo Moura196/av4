ac2517aa52d08ac1eff990381dceb4d749e7db85
brunobpl <brunopaeselima@gmail.com>
2024-07-01 20:03:34 -0300

    Este commit retorna estrutura pré-Malfatti

--- src/pages/Funcionario/index.jsx
+++ src/pages/Funcionario/index.jsx
@@ -1,22 +1,12 @@
-// import React, { useState } from 'react';
-// import FuncionarioList from '../../components/Funcionario/FuncionarioList';
-// import FuncionarioForm from '../../components/Funcionario/FuncionarioForm';
-// import { FuncionarioContainer } from './style';
-
-// export function Funcionario() {
-//   const [selectedFuncionarioId, setSelectedFuncionarioId] = useState(null);
-
-//   const handleSave = () => {
-//     setSelectedFuncionarioId(null);
-//   };
-
-//   return (
-//     <FuncionarioContainer>
-//       <h1>Gestão de Funcionários</h1>
-//       <FuncionarioList />
-//       <FuncionarioForm funcionarioId={selectedFuncionarioId} onSave={handleSave} />
-//     </FuncionarioContainer>
-//   );
-// }
+import { Titulo } from "./style"
 
 
+export function Funcionario() {
+  return (
+    <>
+      <Titulo>
+        Funcionário - Malfatti
+      </Titulo>
+    </>
+  )
+}
--- src/pages/Funcionario/style.js
+++ src/pages/Funcionario/style.js
@@ -1,34 +1,6 @@
 import styled from 'styled-components';
 
-export const FuncionarioContainer = styled.div`
-  margin: 20px;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
+export const Titulo = styled.h1`
+    color: ${({ theme }) => theme.COLORS.RED_YPUA};
 
-export const FuncionarioListItem = styled.li`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  border-bottom: 1px solid #eee;
-
-  &:last-child {
-    border-bottom: none;
-  }
-`;
-
-export const FuncionarioButton = styled.button`
-  background-color: #007bff;
-  color: white;
-  padding: 5px 10px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`;
+`
