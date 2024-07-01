d982bc79cb7db9bb0fe5d35818525891ae3d82d7
LUCAS MALFATTI RIETH <52510447807@udesc.br>
2024-07-01 15:15:02 -0300

    Comentando minhas alterações

--- src/pages/Funcionario/index.jsx
+++ src/pages/Funcionario/index.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import FuncionarioList from '../../components/Funcionario/FuncionarioList';
-import FuncionarioForm from '../../components/Funcionario/FuncionarioForm';
-import { FuncionarioContainer } from './style.js';
+// import React, { useState } from 'react';
+// import FuncionarioList from '../../components/Funcionario/FuncionarioList';
+// import FuncionarioForm from '../../components/Funcionario/FuncionarioForm';
+// import { FuncionarioContainer } from './style';
 
-const Funcionarios = () => {
-  const [selectedFuncionarioId, setSelectedFuncionarioId] = useState(null);
+// export function Funcionario() {
+//   const [selectedFuncionarioId, setSelectedFuncionarioId] = useState(null);
 
-  const handleSave = () => {
-    setSelectedFuncionarioId(null);
-  };
+//   const handleSave = () => {
+//     setSelectedFuncionarioId(null);
+//   };
 
-  return (
-    <FuncionarioContainer>
-      <h1>Gestão de Funcionários</h1>
-      <FuncionarioList />
-      <FuncionarioForm funcionarioId={selectedFuncionarioId} onSave={handleSave} />
-    </FuncionarioContainer>
-  );
-};
+//   return (
+//     <FuncionarioContainer>
+//       <h1>Gestão de Funcionários</h1>
+//       <FuncionarioList />
+//       <FuncionarioForm funcionarioId={selectedFuncionarioId} onSave={handleSave} />
+//     </FuncionarioContainer>
+//   );
+// }
 
-export default Funcionarios;
+
--- src/services/apiFuncionarios.js
+++ src/services/apiFuncionarios.js
@@ -1,23 +1,53 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_URL = 'https://api.funcionario.com';
+// const API_URL = 'https://api.funcionario.com';
 
-export const getFuncionarios = async () => {
-  return axios.get(`${API_URL}/funcionarios`);
-};
+// export const getFuncionarios = async () => {
+//   try {
+//     const response = await axios.get(`${API_URL}/funcionarios`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Erro ao buscar funcionários:", error);
+//     throw error;
+//   }
+// };
 
-export const getFuncionario = async (id) => {
-  return axios.get(`${API_URL}/funcionarios/${id}`);
-};
+// export const getFuncionarioById = async (id) => {
+//   try {
+//     const response = await axios.get(`${API_URL}/funcionarios/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error(`Erro ao buscar funcionário com ID ${id}:`, error);
+//     throw error;
+//   }
+// };
 
-export const createFuncionario = async (funcionario) => {
-  return axios.post(`${API_URL}/funcionarios`, funcionario);
-};
+// export const createFuncionario = async (funcionarioData) => {
+//   try {
+//     const response = await axios.post(`${API_URL}/funcionarios`, funcionarioData);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Erro ao criar funcionário:", error);
+//     throw error;
+//   }
+// };
 
-export const updateFuncionario = async (id, funcionario) => {
-  return axios.put(`${API_URL}/funcionarios/${id}`, funcionario);
-};
+// export const updateFuncionario = async (id, funcionarioData) => {
+//   try {
+//     const response = await axios.put(`${API_URL}/funcionarios/${id}`, funcionarioData);
+//     return response.data;
+//   } catch (error) {
+//     console.error(`Erro ao atualizar funcionário com ID ${id}:`, error);
+//     throw error;
+//   }
+// };
 
-export const deleteFuncionario = async (id) => {
-  return axios.delete(`${API_URL}/funcionarios/${id}`);
-};
+// export const deleteFuncionario = async (id) => {
+//   try {
+//     const response = await axios.delete(`${API_URL}/funcionarios/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.error(`Erro ao deletar funcionário com ID ${id}:`, error);
+//     throw error;
+//   }
+// };
