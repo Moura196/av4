8e98848de4ad9eb3c885037cad532056f997dec9
LUCAS MALFATTI RIETH <52510447807@udesc.br>
2024-07-02 16:01:40 -0300

    Criando o forms do funcionario

--- src/pages/Funcionario/Create/index.jsx
+++ src/pages/Funcionario/Create/index.jsx
@@ -1,43 +1,43 @@
-import { Titulo } from "./style"
 
+import { FormContainer, FormTitle, Form, FormGroup, Label, Input, Button } from './style';
 
-export function Funcionario() {
+export function CreateFuncionario() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para enviar os dados do formulário
+    console.log('Formulário enviado');
+  };
+
   return (
-    <>
-      <h1>Cadastro de Funcionário</h1>
-      <form action="/cadastrarFuncionario" method="POST">
-        <div>
-          <label htmlFor="id_funcionario">ID do Funcionário:</label>
-          <input type="number" id="id_funcionario" name="id_funcionario" required />
-        </div>
-        <div>
-          <label htmlFor="nome">Nome:</label>
-          <input type="text" id="nome" name="nome" maxLength="100" required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" maxLength="60" required />
-        </div>
-        <div>
-          <label htmlFor="telefone">Telefone:</label>
-          <input type="text" id="telefone" name="telefone" maxLength="11" required />
-        </div>
-        <div>
-          <label htmlFor="data_nascimento">Data de Nascimento:</label>
-          <input type="date" id="data_nascimento" name="data_nascimento" required />
-        </div>
-        <div>
-          <label htmlFor="cpf">CPF:</label>
-          <input type="text" id="cpf" name="cpf" maxLength="11" required />
-        </div>
-        <div>
-          <label htmlFor="fk_id_estabelecimento">ID do Estabelecimento:</label>
-          <input type="number" id="fk_id_estabelecimento" name="fk_id_estabelecimento" required />
-        </div>
-        <div>
-          <button type="submit">Cadastrar</button>
-        </div>
-      </form>
-    </>
+    <FormContainer>
+      <FormTitle>Formulário de Cadastro de Funcionário</FormTitle>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="nome">Nome:</Label>
+          <Input type="text" id="nome" name="nome" maxLength="100" required />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="email">Email:</Label>
+          <Input type="email" id="email" name="email" maxLength="60" required />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="telefone">Telefone:</Label>
+          <Input type="text" id="telefone" name="telefone" maxLength="11" required />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="data_nascimento">Data de Nascimento:</Label>
+          <Input type="date" id="data_nascimento" name="data_nascimento" required />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="cpf">CPF:</Label>
+          <Input type="text" id="cpf" name="cpf" maxLength="11" required />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="fk_id_estabelecimento">ID do Estabelecimento:</Label>
+          <Input type="number" id="fk_id_estabelecimento" name="fk_id_estabelecimento" required />
+        </FormGroup>
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+    </FormContainer>
   );
 }
