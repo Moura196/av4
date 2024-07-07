8d6f976a2baa2fa7ebb6f21bb1964f2beb793c3c
Moura196 <gmendesmoura@gmail.com>
2024-07-07 15:13:37 -0300

    Atualização de Cadastro e rotas

---
+++ src/pages/Acomodacao/Cadastro/index.jsx
@@ -0,0 +1,51 @@
+import { DivForm, FormInput, Button } from "./style"
+import React, {useState} from "react"
+
+export function Cadastro () {
+
+    const [nome, setNome] = useState('')
+    const [valorDiaria, setValorDiaria] = useState('')
+    const [limiteOcupantes, setLimiteOcupantes] = useState('')
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        if (name === 'nome') {
+            if (typeof value === 'string') {
+                setNome(value)
+            } else {
+                setNome(value.toString())
+            }
+        }
+        else if (name === 'valorDiaria') setValorDiaria(parseFloat(value))
+        else if (name === 'limiteOcupantes') setLimiteOcupantes(parseInt(value))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    // console.log('Dados do formulário:')
+    // console.log('Nome:', nome)
+    // console.log('Valor da Diária:', valorDiaria)
+    // console.log('Limite de Ocupantes:', limiteOcupantes)
+    
+    // O que está escrito aqui em baixo está funcionando export function Cadastro() {}
+    return (
+        <>
+            <DivForm>
+                <h2>Cadastro de Acomodação</h2>
+                
+                <label htmlFor="nome">Nome da Acomodação:</label>
+                <FormInput type="text" id="nome" name="nome" value={nome} onChange={handleChange} required />
+
+                <label htmlFor="valorDiaria">Valor da Diária:</label>
+                <FormInput type="number" id="valorDiaria" name="valorDiaria" value={valorDiaria} onChange={handleChange} required />
+
+                <label htmlFor="limiteOcupantes">Limite de Ocupantes:</label>
+                <FormInput type="number" id="limiteOcupantes" name="limiteOcupantes" value={limiteOcupantes} onChange={handleChange} required />
+
+                <Button type="submit" onClick={handleSubmit}>Cadastrar Acomocação</Button>
+            </DivForm>            
+        </>
+    )
+}
